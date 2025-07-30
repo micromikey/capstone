3c253cace54b5e9ab42fefa4c81c6e2f584fd2e1
mikeyqwt <mikeyqwt@gmail.com>
2025-07-30 16:01:13 +0800

    first commit

---
+++ HikeThere/resources/views/components/application-logo.blade.php
@@ -0,0 +1,3 @@
+<a href="/">
+    <img src="{{ asset('img/main-logo.png') }}" alt="HikeThere Logo" class="h-16 w-auto mx-auto">
+</a>
---
+++ HikeThere/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+    return redirect('/dashboard');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
+Route::middleware([
+    'auth:sanctum',
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
