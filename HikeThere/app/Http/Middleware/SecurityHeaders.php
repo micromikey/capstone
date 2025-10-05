dafcb5ea80f0efc93a7f25255218c45d042361c9
mikeyqwt <mikeyqwt@gmail.com>
2025-10-06 05:33:09 +0800

    Add Google Cloud Storage to Content Security Policy
    Allow images from storage.googleapis.com so GCS-hosted profile pictures and other uploads can be displayed.

--- HikeThere/app/Http/Middleware/SecurityHeaders.php
+++ HikeThere/app/Http/Middleware/SecurityHeaders.php
@@ -42,7 +42,7 @@ class SecurityHeaders
             $csp = "default-src 'self'; " .
                    "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com https://cdn.jsdelivr.net https://code.iconify.design https://unpkg.com https://code.jquery.com https://cdnjs.cloudflare.com; " .
                    "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.bunny.net https://cdnjs.cloudflare.com; " .
-                   "img-src 'self' data: https: blob:; " .
+                   "img-src 'self' data: https: blob: https://storage.googleapis.com; " .
                    "font-src 'self' https://fonts.gstatic.com https://fonts.bunny.net https://cdnjs.cloudflare.com data:; " .
                    "connect-src 'self' https://maps.googleapis.com https://api.openweathermap.org https://api.iconify.design https://api.simplesvg.com https://api.unisvg.com; " .
                    "frame-src 'self' https://www.google.com;";
