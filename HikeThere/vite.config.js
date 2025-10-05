58275419e2fbff99e81394a81cfdffbd4beb677c
mikeyqwt <mikeyqwt@gmail.com>
2025-10-05 14:00:41 +0800

    Add explicit build config to vite.config.js for Laravel

--- HikeThere/vite.config.js
+++ HikeThere/vite.config.js
@@ -17,4 +17,20 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: [
+                'resources/css/app.css', 
+                'resources/css/map.css', 
+                'resources/css/advanced-trail-map.css',
+                'resources/js/app.js', 
+                'resources/js/map.js', 
+                'resources/js/itinerary-map.js',
+                'resources/js/advanced-trail-map.js',
+                'resources/js/event-poller.js'
+            ]
+        }
+    }
 });
